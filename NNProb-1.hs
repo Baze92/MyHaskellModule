b846dad32706167287f1be66eacdace800516c3e
sabharwalshahbaaz <sabharwalshahbaaz@gmail.com>
2014-07-02 10:48:50 +0800

    Finished Q 31-41

--- NNProb-1.hs
+++ NNProb-1.hs
@@ -2,60 +2,60 @@
 -- Questions sourced from :-> http://www.haskell.org/haskellwiki/99_questions/1_to_10
 module NNProb
 (myLast
-	,myButLast
-	,elementAt
-	,myLength
-	,myReverse
-	,isPalindrome
-	,myFlatten
-	,remove
-	,myCompress
-	,myPack
-	,encode
+    ,myButLast
+    ,elementAt
+    ,myLength
+    ,myReverse
+    ,isPalindrome
+    ,myFlatten
+    ,remove
+    ,myCompress
+    ,myPack
+    ,encode
 
-	) where
-	import Data.Char
-	import Data.List
+    ) where
+    import Data.Char
+    import Data.List
 
-	myLast a = last a
+    myLast a = last a
 
-	myButLast a = head (tail (reverse a))
+    myButLast a = head (tail (reverse a))
 
-	elementAt a b = last (take b a)
+    elementAt a b = last (take b a)
 
-	--myLength a = sum (map (+1) (pdt a))
-	--pdt a = map (*0) (a)
-	myLength :: [a] -> Int -- signature does not mention the type of data in the list
-	myLength = sum . map (\_->1) -- good for us
+    --myLength a = sum (map (+1) (pdt a))
+    --pdt a = map (*0) (a)
+    myLength :: [a] -> Int -- signature does not mention the type of data in the list
+    myLength = sum . map (\_->1) -- good for us
 
-	myReverse :: [a] -> [a]
-	myReverse [] = []
-	myReverse (a:x) = myReverse(x) ++ [a] 
+    myReverse :: [a] -> [a]
+    myReverse [] = []
+    myReverse (a:x) = myReverse(x) ++ [a] 
 
-	isPalindrome a = a == (reverse a)
+    isPalindrome a = a == (reverse a)
 
-	data NestedList a = Elem a | List [NestedList a]
+    data NestedList a = Elem a | List [NestedList a]
 
-	myFlatten :: NestedList a -> [a]
-	myFlatten (Elem a) = [a]
-	myFlatten (List a) = concatMap myFlatten a
+    myFlatten :: NestedList a -> [a]
+    myFlatten (Elem a) = [a]
+    myFlatten (List a) = concatMap myFlatten a
 
 
-	remove _[] = []
-	remove x (a:l) | x == a = remove x l
-				   | otherwise = a : remove x l
+    remove _[] = []
+    remove x (a:l) | x == a = remove x l
+                   | otherwise = a : remove x l
 
-	myCompress [] = []
-	myCompress (a:xs) = a : (myCompress $ dropWhile (==a) xs)
+    myCompress [] = []
+    myCompress (a:xs) = a : (myCompress $ dropWhile (==a) xs)
 
-	-- ok, I know this is not what we are looking for but yeah I will work on this later
-	-- ****************
-	myPack b = group b
-	-- ****************
+    -- ok, I know this is not what we are looking for but yeah I will work on this later
+    -- ****************
+    myPack b = group b
+    -- ****************
 
-	encode [] = []
-	encode xs = [((length a) , (head a)) | a <- group xs]
-			
+    encode [] = []
+    encode xs = [((length a) , (head a)) | a <- group xs]
+            
 
 
 
--- NNProb-4.hs
+++ NNProb-4.hs
@@ -1,12 +1,29 @@
 module NNProb4
 (isPrime
-	,myGcd
-	,coprime
-	) where
-		import Data.List
+,myGcd
+,coprime
+,totient
+,totient'
+,primes
+,goldbach
+,goldbachList
+) where
+	import Data.List
+	
+	isPrime a = if length (dropWhile (==0) (tail [ ans | c <- [1..a], let ans = if a `rem` c == 0 then c else 0])) > 2 then False else True
+	
+	myGcd a b = maximum ( [ ans | c <- [1..a], let ans = if a `rem` c == 0 then c else 0] `intersect` [ ans | c <- [1..b], let ans = if b `rem` c == 0 then c else 0])
+	
+	coprime a b = if myGcd a b == 1 then True else False
+	
+	totient a = foldl (+) 0 [ ans | b <- [1..a], let ans = if coprime a b then 1 else 0]
+	
+	totient' a = length [ b | b <- [1..a], coprime a b] --figured it out late
+	
+	primes a b = [ c | c <- [a..b], isPrime c ]
+	
+	goldbach a  
+			| a `rem` 2 == 0   = head [(x,y) | x <- (primes 2 a), let y = a-x, isPrime y ]
+			| otherwise  = error "Does not hold for odd numbers"
 
-		isPrime a = if length (dropWhile (==0) (tail [ ans | c <- [1..a], let ans = if a `rem` c == 0 then c else 0])) > 2 then False else True
-
-		myGcd a b = maximum ( [ ans | c <- [1..a], let ans = if a `rem` c == 0 then c else 0] `intersect` [ ans | c <- [1..b], let ans = if b `rem` c == 0 then c else 0])
-
-		coprime a b = if myGcd a b == 1 then True else False
+	goldbachList a b = [goldbach c | c <- [a..b], even(c)]
